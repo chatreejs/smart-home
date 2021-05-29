a6e47eea0da10260a66a99da2cf36d29627d5545
CHANON TREEMETH <chanon.t@cdg.co.th>
2021-05-30 02:47:13 +0700

    refactor: update main menu layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,59 @@
-import { Component, Inject, OnInit } from '@angular/core'
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core'
 import { Title } from '@angular/platform-browser'
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
+import { fromEvent, Observable, Subscription } from 'rxjs'
 import { filter, map } from 'rxjs/operators'
 
 import { GeneralEnvironmentConfig } from './core/environment-config'
 import { GENERAL_ENVIRONMENT_CONFIG } from './core/provider-name-token'
 
+const listOfAvatarColor = [
+  "#FFB900",
+  "#D83B01",
+  "#B50E0E",
+  "#E81123",
+  "#B4009E",
+  "#5C2D91",
+  "#0078D7",
+  "#00B4FF",
+  "#008272",
+  "#107C10"
+]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  isCollapsed: boolean
+  innerWidth: number
+  user: any
+  avatarName: string
+  avatarColor: string
+
+  resizeObservable: Observable<Event>
+  resizeSubscription: Subscription
 
   public constructor(
     private titleService: Title,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     @Inject(GENERAL_ENVIRONMENT_CONFIG) private generalEnvironmentConfig: GeneralEnvironmentConfig) {
+    this.isCollapsed = false
+    this.avatarName = ""
+    this.avatarColor = ""
+
+    // Subscribe window inner width
+    this.innerWidth = window.innerWidth
+    this.resizeObservable = fromEvent(window, 'resize')
+    this.resizeSubscription = this.resizeObservable.subscribe(e => {
+      this.innerWidth = window.innerWidth
+    })
   }
 
   public ngOnInit(): void {
     const appTitle = this.generalEnvironmentConfig.appName
+    // Set dynamic title by page
     this.router
       .events.pipe(
         filter(event => event instanceof NavigationEnd),
@@ -38,5 +70,33 @@ export class AppComponent implements OnInit {
       ).subscribe((title: string) => {
         this.titleService.setTitle(title)
       })
+
+    // fake
+    this.user = {
+      firstName: "Admin",
+      lastName: "A"
+    }
+    this.avatarName = this.calculateAvatarName(this.user.firstName, this.user.lastName)
+    this.avatarColor = this.calculateAvatarColor(this.user.firstName)
   }
+
+  public calculateAvatarName(firstName: string, lastName: string): string {
+    const firstLetter = firstName.substr(0, 1)
+    const lastLetter = lastName.substr(0, 1)
+    return firstLetter + lastLetter
+  }
+
+  public calculateAvatarColor(firstName: string): string {
+    let sum = 0
+    for (let i = 0; i < firstName.length; i++) {
+      sum += firstName.charCodeAt(i);
+    }
+    let colorId = sum % listOfAvatarColor.length
+    return listOfAvatarColor[colorId]
+  }
+
+  public ngOnDestroy(): void {
+    this.resizeSubscription.unsubscribe()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout'
 import { NzMenuModule } from 'ng-zorro-antd/menu'
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb'
 import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzTypographyModule } from 'ng-zorro-antd/typography'
 import { NzTableModule } from 'ng-zorro-antd/table'
 import { NzDividerModule } from 'ng-zorro-antd/divider'
 import { NzTagModule } from 'ng-zorro-antd/tag'
@@ -23,6 +24,8 @@ import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm'
 import { NzInputModule } from 'ng-zorro-antd/input'
 import { NzMessageModule } from 'ng-zorro-antd/message'
+import { NzAvatarModule } from 'ng-zorro-antd/avatar'
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown'
 
 import { AppComponent } from './app.component'
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
@@ -55,6 +58,7 @@ registerLocaleData(th)
     NzMenuModule,
     NzBreadCrumbModule,
     NzGridModule,
+    NzTypographyModule,
     NzTableModule,
     NzDividerModule,
     NzTagModule,
@@ -62,6 +66,8 @@ registerLocaleData(th)
     NzPopconfirmModule,
     NzInputModule,
     NzMessageModule,
+    NzAvatarModule,
+    NzDropDownModule,
   ],
   providers: [
     { provide: GENERAL_ENVIRONMENT_CONFIG, useValue: environment },
